827ca77260f3de72a8ccb13abc45f84613035e36
IceHawk <mukherjeeayush97@gmail.com>
2024-10-28 23:48:07 +0530

    Files added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Body from './components/Body/Body';
+
+
+function App(){
+    return (
+        <>
+            <Body />
+        </>
+    );
+}
+
+
+export default App;
